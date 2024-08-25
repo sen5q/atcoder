ba5a01f4870b17f7aac24cf6305d646832b65e94
sen5q <95946173+sen5q@users.noreply.github.com>
2024-08-25 11:28:09 +0900

    update _note.py

--- _note.py
+++ _note.py
@@ -2,22 +2,22 @@
 #整数
 #小数
 
-# 1行1列
-s = input()
-n = int(input())
-f = float(input())
+#1個　
+input()
+int(input())
+float(input())
 
-# 1行複数列
-a, b = input().split()
-n, y, z = map(int, input().split())
-h, m, s = map(float, input().split())
+#1行複数
+input().split()
+map(int, input().split())
+map(float, input().split())
 
-# 1行配列
-a = input().split()
-a = list(map(int, input().split()))
-a = list(map(float, input().split()))
+#1行の配列
+input().split()
+list(map(int, input().split()))
+list(map(float, input().split()))
 
 #複数行配列
-a = [input().split() for _ in range(n)]
-a = [list(map(int, input().split())) for _ in range(n)]
-a = [list(map(float, input().split())) for _ in range(n)]
+[input().split() for _ in range(n)]
+[list(map(int, input().split())) for _ in range(n)]
+[list(map(float, input().split())) for _ in range(n)]
